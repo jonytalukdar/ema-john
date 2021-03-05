7a6f6f1322ece773d5d4f31815b8d7025503ac65
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-05 23:34:51 +0600

    remove some file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import logo from '../../images/logo.png';
-import './Header.css';
-import { useAuth } from "../Auth/use-auth";
-import {
-    Link
-  } from "react-router-dom";
-
+import logo from '../../images/logo.png'
+import '../Header/Header.css'
 const Header = () => {
-    const auth = useAuth();
-    console.log(auth);
     return (
-        <div className="header">
+        <div className='header'>
             <img src={logo} alt=""/>
             <nav>
                 <a href="/shop">Shop</a>
                 <a href="/review">Order Review</a>
-                <a href="/inventory">Manage Inventory</a>
-                {auth && auth.user && <span style={{color:'white'}}>{auth.user.email}</span>}
-                {
-                    auth && !auth.user ? <button onClick={()=>auth.signinWithGoogle()}>Sign in</button> 
-                    : <button onClick={() => auth.signout()}>Sign Out</button>
-                }
-                
-                
+                <a href="/manage">Manage Inventory</a>
             </nav>
         </div>
     );
