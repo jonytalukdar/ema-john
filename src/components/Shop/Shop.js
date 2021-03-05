e299c249ff862a045e65098931142f491da5568a
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-06 01:07:16 +0600

    save localstorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,15 +3,19 @@ import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
+import product from '../Product/Product';
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products, setProducts] = useState(first10);
   const [cart, setCart] = useState([]);
 
   const handleAddProduct = (product) => {
-    console.log('Product added', product);
     const newCart = [...cart, product];
     setCart(newCart);
+    const sameProduct = newCart.filter((pd) => pd.key === product.key);
+    const count = sameProduct.length;
+    addToDatabaseCart(product.key, count);
   };
 
   return (
@@ -19,6 +23,7 @@ const Shop = () => {
       <div className="product-container">
         {products.map((pd) => (
           <Product
+            key={pd.key}
             showAddToCart={true}
             handleAddProduct={handleAddProduct}
             product={pd}
