fb4adc9d87af00d3e80533a4c648c346b061fad8
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-06 00:22:01 +0600

    added useparams

--- src/components/productDetails/ProductDetails.js
+++ src/components/productDetails/ProductDetails.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import fakeData from '../../fakeData';
 
 const ProductDetails = () => {
+  const { productKey } = useParams();
+  const product = fakeData.find((pd) => pd.key === productKey);
+  console.log(product);
   return (
     <div>
-      <h1>Product details comming soon</h1>
+      <h1> {productKey}Product details comming soon</h1>
     </div>
   );
 };
