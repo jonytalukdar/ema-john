3f63726a2e88642c2dcc9c4836b5baa2128fd16c
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-06 15:21:47 +0600

    added shop sync

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDatabaseCart } from '../../utilities/databaseManager';
+import {
+  addToDatabaseCart,
+  getDatabaseCart,
+} from '../../utilities/databaseManager';
 import product from '../Product/Product';
 const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products, setProducts] = useState(first10);
   const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const saveCart = getDatabaseCart();
+    const productKeys = Object.keys(saveCart);
+    const previousCart = productKeys.map((existingKey) => {
+      const product = fakeData.find((pd) => pd.key === existingKey);
+      product.quantity = saveCart[existingKey];
+      return product;
+    });
+    setCart(previousCart);
+  }, []);
 
   const handleAddProduct = (product) => {
     const toBeAddedKey = product.key;
